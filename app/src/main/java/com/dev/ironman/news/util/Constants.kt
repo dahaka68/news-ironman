1777139f36ccf1693a8f1ffa39c047e081d7417f
Ink <eugeny.yushkevich@gmail.com>
2018-03-25 02:50:11 +0300

    Improved Parse const mistake

--- app/src/main/java/com/dev/ironman/news/util/Constants.kt
+++ app/src/main/java/com/dev/ironman/news/util/Constants.kt
@@ -1,7 +1,7 @@
-package com.dev.ironman.news
+package com.dev.ironman.news.util
 
 
 const val BASE_URL = "https://newsapi.org/v2/"
 const val API_KEY = "ed410f2fab8a4663a19c5d2e8fe6850a"
 const val HEADER = "X-Api-Key"
-const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
+const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
