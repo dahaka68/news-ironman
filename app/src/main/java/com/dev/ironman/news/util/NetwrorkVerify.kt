b5713810a617f836f77c4647cb07d884a8b04876
Ink <eugeny.yushkevich@gmail.com>
2018-03-25 01:48:41 +0300

    added netWork Verifying class

--- app/src/main/java/com/dev/ironman/news/util/NetwrorkVerify.kt
+++ app/src/main/java/com/dev/ironman/news/util/NetwrorkVerify.kt
@@ -1,7 +1,26 @@
 package com.dev.ironman.news.util
 
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
 /**
  * Created by User on 25.03.2018.
  */
-class NetwrorkVerify {
+class NetwrorkVerify(val context: Application) {
+
+	//TODO better (it's not complete checkeing if the internet there's
+	fun checkInternet(): Boolean {
+		val wifiCheck: NetworkInfo?
+		val connectionManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+		wifiCheck = connectionManager.activeNetworkInfo
+
+		wifiCheck?.let {
+			if (wifiCheck.type == ConnectivityManager.TYPE_WIFI || wifiCheck.type == ConnectivityManager.TYPE_MOBILE)
+				return true
+		}
+
+		return false
+	}
 }
